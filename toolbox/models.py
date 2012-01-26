6c46c9d0aeef378cb612749507e549d1653f9254
Tomek Paczkowski <tomek@hauru.eu>
2012-01-26 19:18:17 +0100

    Bad mood commit

--- toolbox/models.py
+++ toolbox/models.py
@@ -2,33 +2,3 @@
 
 class ModelError(Exception):
     """Standard model error"""
-
-
-class Project(object):
-    def __init__(self, title):
-        self.title = title
-
-
-class Task(object):
-    def __init__(self, text, description=""):
-        self.text = text
-        self.description = description
-        self.parent = None
-
-
-class Story(Task):
-    def __init__(self, text, description=""):
-        super(Story, self).__init__(text, description)
-        self.subtasks = []
-
-    def add_subtask(self, subtask):
-        if subtask.parent is self:
-            return
-        if subtask.parent:
-            subtask.parent.remove_subtask(subtask)
-        self.subtasks.append(subtask)
-        subtask.parent = self
-
-    def remove_subtask(self, subtask):
-        self.subtasks.remove(subtask)
-        subtask.parent = None
---
+++ user_stories.rst
@@ -0,0 +1,16 @@
+Users stories for Toolbox
+=========================
+
+
+Users
+-----
+
+As a newcomer I want to create account identified by email
+
+
+Projects
+--------
+
+As a new user I want to create new project with name and slug
+As a user I want to see list of all created projects.
+As a project user I want to add more users to the project.
